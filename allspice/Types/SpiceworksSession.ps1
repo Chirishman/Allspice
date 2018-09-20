9b49de12ec9bccfaaf7ea58dd1a8ce562a577fc1
Chirishman <Chirishman@users.noreply.github.com>
2018-09-20 16:06:40 -0400

    forgot the self-constructor

--- allspice/Types/SpiceworksSession.ps1
+++ allspice/Types/SpiceworksSession.ps1
@@ -65,6 +65,9 @@ class SpiceworksSession {
     SpiceworksSession([string]$server,[pscredential]$credential){
         $this.NewSession($server,$credential,$false,$null)
     }
+    SpiceworksSession([SpiceworksSession]$obj){
+        $this = $obj
+    }
 
     [void] Connect(){
         add-type "using System.Net; using System.Security.Cryptography.X509Certificates; public class TrustAllCertsPolicy : ICertificatePolicy { public bool CheckValidationResult( ServicePoint srvPoint, X509Certificate certificate, WebRequest request, int certificateProblem) { return true; } }"
